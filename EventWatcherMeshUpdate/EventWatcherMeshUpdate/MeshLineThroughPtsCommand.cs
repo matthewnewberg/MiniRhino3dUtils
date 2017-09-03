0f392578842001d16b5abc8d3f92073e36f7165f
Matthew Newberg <mnewberg@gmail.com>
2017-09-03 15:33:04 -0400

    Add new MeshLineThroughPts command

---
+++ EventWatcherMeshUpdate/EventWatcherMeshUpdate/MeshLineThroughPtsCommand.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+
+namespace EventWatcherMeshUpdate
+{
+    [System.Runtime.InteropServices.Guid("4281794f-0918-45da-b0a8-ad8550bb4e73")]
+    public class MeshLineThroughPtsCommand : Command
+    {
+        static MeshLineThroughPtsCommand _instance;
+        public MeshLineThroughPtsCommand()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the MeshLineThroughPtsCommand command.</summary>
+        public static MeshLineThroughPtsCommand Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "MeshLineThroughPts"; }
+        }
+
+        private const int HISTORY_VERSION = 201700903;
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            const Rhino.DocObjects.ObjectType filter = Rhino.DocObjects.ObjectType.Mesh;
+            Rhino.DocObjects.ObjRef objref;
+            Rhino.Commands.Result rc = Rhino.Input.RhinoGet.GetOneObject("Select mesh to add line to", false, filter, out objref);
+            if (rc != Rhino.Commands.Result.Success || objref == null)
+                return rc;
+
+            Rhino.Geometry.Mesh mesh = objref.Mesh();
+            if (null == mesh || !mesh.IsValid)
+                return Rhino.Commands.Result.Failure;
+
+            Line fittedLine = new Line();
+            Line.TryFitLineToPoints(mesh.Vertices.ToPoint3dArray(), out fittedLine);
+
+            Rhino.DocObjects.HistoryRecord history = new Rhino.DocObjects.HistoryRecord(this, HISTORY_VERSION);
+            WriteHistory(history, objref);
+            
+            doc.Objects.AddCurve(new LineCurve(fittedLine), null, history, false);
+
+            doc.Views.Redraw();
+
+            return Result.Success;
+        }
+
+
+        protected override bool ReplayHistory(Rhino.DocObjects.ReplayHistoryData replay)
+        {
+            Rhino.DocObjects.ObjRef objref = null;
+
+            if (!ReadHistory(replay, ref objref))
+                return false;
+
+            Rhino.Geometry.Mesh mesh = objref.Mesh();
+            if (null == mesh)
+                return false;
+
+            if (replay.Results.Length != 1)
+                return false;
+
+            Line fittedLine = new Line();
+            Line.TryFitLineToPoints(mesh.Vertices.ToPoint3dArray(), out fittedLine);
+
+            replay.Results[0].UpdateToCurve(new LineCurve(fittedLine), null);
+
+            return true;
+        }
+
+        private bool WriteHistory(Rhino.DocObjects.HistoryRecord history, Rhino.DocObjects.ObjRef objref)
+        {
+            if (!history.SetObjRef(0, objref))
+                return false;
+
+            return true;
+        }
+
+        private bool ReadHistory(Rhino.DocObjects.ReplayHistoryData replay, ref Rhino.DocObjects.ObjRef objref)
+        {
+            if (HISTORY_VERSION != replay.HistoryVersion)
+                return false;
+
+            objref = replay.GetRhinoObjRef(0);
+            if (null == objref)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ EventWatcherMeshUpdate/EventWatcherMeshUpdate/miniMeshLineThroughPtsCommand.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+
+namespace EventWatcherMeshUpdate
+{
+    [System.Runtime.InteropServices.Guid("60f120f0-f729-4cf9-8768-29c01447d0d0")]
+    public class miniMeshLineThroughPtsCommand : Command
+    {
+        bool Enabled = false;
+
+        bool inReplace = false;
+
+        Dictionary<Guid, Guid> objectLookup = new Dictionary<Guid, Guid>();
+
+        static miniMeshLineThroughPtsCommand _instance;
+        public miniMeshLineThroughPtsCommand()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the MeshLineThroughPts command.</summary>
+        public static miniMeshLineThroughPtsCommand Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "miniMeshLineThroughPts"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+
+            Enabled = !Enabled;
+
+            if (Enabled)
+            {
+                RhinoDoc.AddRhinoObject += RhinoObjectAdded;
+                RhinoDoc.DeleteRhinoObject += RhinoObjectDeleted;
+                RhinoDoc.ReplaceRhinoObject += RhinoObjectReplace;
+
+                foreach (var obj in doc.Objects)
+                {
+                    if (!obj.IsDeleted)
+                        continue;
+
+                    var mesh = obj.Geometry as Mesh;
+                    if (mesh != null && mesh.IsValid)
+                    {
+                        AddThroughPts(obj, obj.Attributes.ObjectId, doc);
+
+                    }
+                }
+                
+            }
+            else
+            {
+                RhinoDoc.AddRhinoObject -= RhinoObjectAdded;
+                RhinoDoc.DeleteRhinoObject -= RhinoObjectDeleted;
+                RhinoDoc.ReplaceRhinoObject -= RhinoObjectReplace;
+            }
+
+            RhinoApp.WriteLine("miniMeshLineThroughPts:" + Enabled);
+
+            return Result.Success;
+        }
+
+
+        private void RhinoObjectDeleted(Object sender, RhinoObjectEventArgs e)
+        {
+            if (inReplace)
+                return;
+
+            if (objectLookup.ContainsKey(e.ObjectId))
+            {
+                e.TheObject.Document.Objects.Delete(objectLookup[e.ObjectId], true);
+
+                objectLookup.Remove(e.ObjectId);
+            }
+        }
+
+        private void RhinoObjectReplace(object sender, RhinoReplaceObjectEventArgs e)
+        {
+            inReplace = true;
+        }
+
+        private void RhinoObjectAdded(Object sender, RhinoObjectEventArgs e)
+        {
+            var doc = e.TheObject.Document;
+            if (Enabled)
+            {
+                AddThroughPts(e.TheObject, e.ObjectId, doc);
+            }
+
+            inReplace = false;
+        }
+
+        private void AddThroughPts(Rhino.DocObjects.RhinoObject obj, Guid objectId,  RhinoDoc doc)
+        {
+            Rhino.DocObjects.MeshObject meshObject = obj as MeshObject;
+
+            if (meshObject != null && meshObject.IsValid)
+            {
+                var objectMesh = obj.Geometry as Mesh;
+
+                Line fittedLine = new Line();
+                Line.TryFitLineToPoints(objectMesh.Vertices.ToPoint3dArray(), out fittedLine);
+
+                bool replacedResult = false;
+
+                if (objectLookup.ContainsKey(objectId))
+                {
+                    replacedResult = doc.Objects.Replace(objectLookup[objectId], new LineCurve(fittedLine));
+                    doc.Objects.ModifyAttributes(objectLookup[objectId], obj.Attributes, true);
+                }
+
+                if (!replacedResult)
+                    objectLookup[objectId] = doc.Objects.Add(new LineCurve(fittedLine), obj.Attributes);
+
+            }
+        }
+    }
+}
